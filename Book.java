79cc66150d86c2ef3925c919b9441dbb4f13d532
Gearglazer424 <G00218336@W213-10.academic.raritanval.edu>
2015-09-21 20:47:56 -0400

    2.84, 2.85, 2.86

--- Book.java
+++ Book.java
@@ -11,15 +11,16 @@ class Book
     // The fields.
     private String author;
     private String title;
-
+    private int pages;
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        bookPages = pages;
     }
 
     public String getAuthor()
@@ -30,4 +31,19 @@ class Book
    {
        return title;
    }
-}
+   public void printAuthor()
+   {
+       System.out.println(author);
+    }
+    public void printTitle()
+    {
+        System.out.println(title);
+    }
+    public void printDetails()
+    {
+        System.out.println("Title" + title);
+        System.out.println("Author" + author);
+        System.out.println("Pages" + pages);
+    }
+}   
+        
