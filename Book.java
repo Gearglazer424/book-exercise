6a43a2c71a727469329588d760be3d7c990bf6b0
Gearglazer424 <G00218336@W213-09.academic.raritanval.edu>
2015-09-28 20:33:03 -0400

    2.88

--- Book.java
+++ Book.java
@@ -46,6 +46,12 @@ class Book
         System.out.println("Title" + title);
         System.out.println("Author" + author);
         System.out.println("Pages" + pages);
+        if(refNumber.length() > 1) 
+            {    
+                System.out.print("ZZZ");
+            }
+            else
+                System.out.print(refNumber);
     }
     public void setRefNumber(String ref)
     {
